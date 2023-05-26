f4a0f85825069a5985c12f7198b96ed13190779a
MLxpezz <mlopez_098@hotmail.com>
2023-05-26 16:44:59 -0600

    new url

--- src/tools/fetch.js
+++ src/tools/fetch.js
@@ -7,13 +7,13 @@ export async function request(lat, long) {
 
 
 export async function requestName(cityName, state) {
-    const data = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${state}&limit=10&appid=64cd917c76428239bd793e7c69674f03`);
+    const data = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${state}&limit=10&appid=64cd917c76428239bd793e7c69674f03`);
 
     return data.json();
 }
 
 export async function forecastRequest(lat, long) {
-  const data = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=64cd917c76428239bd793e7c69674f03&units=metric&lang=es`);
+  const data = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=64cd917c76428239bd793e7c69674f03&units=metric&lang=es`);
 
   return data.json();
 }
