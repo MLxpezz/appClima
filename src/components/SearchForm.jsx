f093440fb284128f031e7456aa63928435354cd9
MLxpezz <mlopez_098@hotmail.com>
2023-06-01 22:25:52 -0600

    commit

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { request, requestName, forecastRequest } from "../tools/fetch";
+import { request, forecastRequest, requestCity } from "../tools/fetch";
 import InfoWeather from "./InfoWeather";
 import ForeCastCard from "./ForeCastCard";
+import { options, error } from "../tools/fetch";
 import "../styles/searchForm.css";
 
 const SearchForm = () => {
@@ -11,19 +12,20 @@ const SearchForm = () => {
 
   const iconUrl = "https://openweathermap.org/img/wn/";
 
+  const position = async (pos) => {
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+    const promise = await request(latitude, longitude);
+    setClima(promise);
+    const forecast = await forecastRequest(latitude, longitude);
+    setFore(forecast.list);
+  };
+
   useEffect(() => {
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        const promise = await request(latitude, longitude);
-        setClima(promise);
-  
-        const forecast = await forecastRequest(latitude, longitude);
-        setFore(forecast.list);
-      });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position, error, options);
     } else {
-      console.error('Error en la geolocalizacion.');
+      console.error("Error en la geolocalizacion.");
     }
   }, []);
 
@@ -39,9 +41,11 @@ const SearchForm = () => {
         return;
       }
 
-      const data = await requestName(name);
-      const newCity = await request(data[0].lat, data[0].lon);
-      const forecast = await forecastRequest(data[0].lat, data[0].lon);
+      const newCity = await requestCity(name);
+      const forecast = await forecastRequest(
+        newCity.coord.lat,
+        newCity.coord.lon
+      );
       setClima(newCity);
       setFore(forecast.list);
       setName("");
--- src/tools/fetch.js
+++ src/tools/fetch.js
@@ -5,15 +5,27 @@ export async function request(lat, long) {
   return data.json();
 }
 
-
-export async function requestName(cityName, state) {
-    const data = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${state}&limit=10&appid=64cd917c76428239bd793e7c69674f03`);
-
-    return data.json();
+export async function requestCity(cityName, state) {
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${state}&appid=64cd917c76428239bd793e7c69674f03&units=metric&lang=es`
+  );
+  return data.json();
 }
 
 export async function forecastRequest(lat, long) {
-  const data = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=64cd917c76428239bd793e7c69674f03&units=metric&lang=es`);
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=64cd917c76428239bd793e7c69674f03&units=metric&lang=es`
+  );
 
   return data.json();
 }
+
+export const options = {
+  enableHighAccuracy: true,
+  timeout: 4000,
+  maximumAge: 0,
+};
+
+export const error = (err) => {
+  console.error("Error obteniendo la ubicacion", err);
+};
